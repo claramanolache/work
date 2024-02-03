b4e96ab0992164b7e171e42bca0c41accb5242aa
clara <clara.manolache@gmail.com>
2024-02-03 09:46:37 -0800

    works for one half of the field

--- src/CheckCordInTrussUsingLineFormula.java
+++ src/CheckCordInTrussUsingLineFormula.java
@@ -1,8 +1,16 @@
 public class CheckCordInTrussUsingLineFormula {
     public static boolean check_Using_lines(double x, double y){
         if (x>125.024 && x<168.78 && 222.934>y && 100.316<y){
-            if (below_top_left_line(x,y) && above_bottom_left_line(x,y) && above_bottom_left_small_triangle(x ,y) && below_top_left_small_triangle(x, y)){
+            if (below_top_left_line(x,y) && above_bottom_left_line(x,y) && above_bottom_left_triangle(x ,y) && below_top_left_triangle(x, y)){
                 return true;
+            }
+        }
+        return false;
+    }
+    public static boolean left_check_Using_lines(double x, double y){
+        if (x > 420.035 && x < 526.226 && y>100.316 && y<222.934){
+            if (below_top_right_triangle(x,y) && above_bottom_right_triangle(x,y) && below_top_right_line(x,y) && above_bottom_right_line(x,y)){
+                return (true);
             }
         }
         return false;
@@ -23,7 +31,7 @@ public class CheckCordInTrussUsingLineFormula {
         }
         return false;
     }
-    public static boolean below_top_left_small_triangle (double x, double y){
+    public static boolean below_top_left_triangle (double x, double y){
         double interception_of_line = (y-222.934)/(-.577261357);
         interception_of_line += 218.822;
         if (interception_of_line>x){
@@ -31,7 +39,7 @@ public class CheckCordInTrussUsingLineFormula {
         }
         return false;
     }
-    public static boolean above_bottom_left_small_triangle (double x, double y){
+    public static boolean above_bottom_left_triangle (double x, double y){
         double interception_of_line = (y-107.47)/(0.577261357);
         interception_of_line += 231.215;
         if (interception_of_line>x){
@@ -39,9 +47,36 @@ public class CheckCordInTrussUsingLineFormula {
         }
         return false;
     }
-    public static boolean left_check_Using_lines(double x, double y){
-        if (x > 420.035 && x < 526.226 && y>100.316 && y<222.934){
-            return (true);
+
+    public static boolean below_top_right_triangle (double x, double y){
+        double interception_of_line = (y-107.47)/(0.577261357);
+        interception_of_line += 231.215;
+        if (interception_of_line<x){
+            return true;
+        }
+        return false;
+    }
+    public static boolean below_top_right_line (double x, double y){
+        double interception_of_line = (y-222.934)/(-0.57734204793);
+        interception_of_line += 432.428;
+        if (interception_of_line>x){
+            return true;
+        }
+        return false;
+    }
+    public static boolean above_bottom_right_line (double x, double y){
+        double interception_of_line = (y-100.316)/(0.577347064969);
+        interception_of_line += 432.428;
+        if (interception_of_line>x){
+            return true;
+        }
+        return false;
+    }
+    public static boolean above_bottom_right_triangle (double x, double y){
+        double interception_of_line = (y-107.47)/(-0.577261357218);
+        interception_of_line += 420.035;
+        if (interception_of_line<x){
+            return true;
         }
         return false;
     }
--- src/CheckCordInTrussUsingLineFormulaTest.java
+++ src/CheckCordInTrussUsingLineFormulaTest.java
@@ -4,18 +4,30 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CheckCordInTrussUsingLineFormulaTest {
     @Test
-    void positiveTest() {
+    void left_positiveTest() {
         boolean test = CheckCordInTrussUsingLineFormula.check_Using_lines(150, 150);
         assertTrue(test);
     }
     @ Test
-    void positiveTest2() {
+    void left_positiveTest2() {
         boolean test = CheckCordInTrussUsingLineFormula.check_Using_lines(200, 175);
         assertTrue(test);
     }
     @ Test
-    void negativeTest() {
+    void left_negativeTest() {
         boolean test = CheckCordInTrussUsingLineFormula.check_Using_lines(200, 100);
+        assertFalse(test);
+    }
+
+    @Test
+    void right_positiveTest() {
+        boolean test = CheckCordInTrussUsingLineFormula.left_check_Using_lines(450,200);
+        assertTrue(test);
+    }
+
+    @Test
+    void right_negativeTest() {
+        boolean test = CheckCordInTrussUsingLineFormula.left_check_Using_lines(450,350);
         assertFalse(test);
     }
 
